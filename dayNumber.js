8c9eff9e20764bd4abae3bac120e5772df2d3ed0
artemkukol <arhi2319@gmail.com>
2023-04-15 23:48:11 +0300

    dayNumber
    вывод дня недели по введённому номеру недели

---
+++ dayNumber.js
@@ -0,0 +1,19 @@
+const dayNumber = 3; //задайте номер дня
+
+if (dayNumber === 1) {
+   console.log('Понедельник');
+}else if (dayNumber === 2) {
+   console.log('Вторник');
+}else if (dayNumber === 3) {
+   console.log('Среда');
+}else if (dayNumber === 4) {
+   console.log('Четверг');
+}else if (dayNumber === 5) {
+   console.log('Пятница');
+}else if (dayNumber === 6) {
+   console.log('Суббота');
+}else if (dayNumber === 7) {
+   console.log('Воскресенье');
+}else {
+   console.log('Некорректный номер дня')
+}
